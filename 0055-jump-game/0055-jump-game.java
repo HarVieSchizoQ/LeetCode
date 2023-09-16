5d2b6d7c14a6975ad126d08a94003fa87410fcfd
Harvie Purgatorio <116258847+HarVieSchizoQ@users.noreply.github.com>
2023-09-15 19:02:17 -0700

    Time: 1 ms (99.97%), Space: 44.4 MB (36.82%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,33 +1,31 @@
 class Solution {
     public boolean canJump(int[] nums) {
-//         if (nums.length == 1) 
-//             return true;
         
-//         if (nums[nums.length - 2] + nums.length - 2 < nums.length - 1)
-//             return false;
         
-//         int fIndex = nums.length - 2;
+        int fIndex = nums.length - 1;
         
-//         for (int i = nums.length - 3; i >= 0; i--) {
-//              if (nums[i] + i >= fIndex) 
-//                     fIndex = i;
+        for (int i = nums.length - 2; i >= 0; i--) {
+             if (nums[i] + i >= fIndex) 
+                    fIndex = i;
              
-//         }
+        }
+        
+        return fIndex == 0;
         
 //         return fIndex == 0 ? true: false;
         
-        int maxReach = 0;
+//         int maxReach = 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (i > maxReach)
-                return false;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (i > maxReach)
+//                 return false;
             
-            maxReach = Math.max(maxReach, nums[i] + i);
+//             maxReach = Math.max(maxReach, nums[i] + i);
             
-            if (maxReach >= nums.length - 1)
-                return true;
-        }
+//             if (maxReach >= nums.length - 1)
+//                 return true;
+//         }
         
-        return false;
+//         return false;
     }
 }
