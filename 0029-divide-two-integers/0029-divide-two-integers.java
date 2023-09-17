0ba45990070dcc75a77c2d6798c3cce59ac1512e
Harvie Purgatorio <116258847+HarVieSchizoQ@users.noreply.github.com>
2023-09-17 12:44:12 -0700

    Time: 0 ms (100.00%), Space: 39.7 MB (39.49%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,8 @@
+class Solution {
+    public static int divide(int dividend, int divisor) {
+
+        if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE; 
+        int res = dividend / divisor;
+        return res;
+    }
+}
