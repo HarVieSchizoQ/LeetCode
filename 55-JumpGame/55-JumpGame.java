f7dd04ce43d8029bb8b492861cc75a2d2a8a9a30
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-02-02 20:48:41 -0800

    Create file

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+
+            if (nums[i] + i >= goal)
+        } 
+    }
+                goal--;
+
+        return goal == 0 ? true : false;
+        if (nums.length == 1)
+            return true;
+
+}
+[
