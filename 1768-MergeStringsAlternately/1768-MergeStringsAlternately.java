2e728a30f7453c19a6fea0cfc1df9b9a71eafef4
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-02-16 03:00:30 -0800

    Create file

---
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -0,0 +1,20 @@
+            } else {
+                p1++;
+                res += word2.charAt(p2);
+                p2++;
+            }
+
+            count++;
+        }
+
+            if (count % 2 == 0) {
+                res += word1.charAt(p1);
+        while (p1 < word1.length() && p2 < word2.length()) {
+        while (p1 < word1.length()) {
+            res += word1.charAt(p1);
+            p1++;
+        }
+
+        while (p2 < word2.length()) {
+            res += word2.charAt(p2);
+"
