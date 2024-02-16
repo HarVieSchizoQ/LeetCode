07a1850a59f9ee877ac2eb7ec13699cdb8594938
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-02-16 03:11:28 -0800

    Create file

---
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,19 @@
+    }
+
+    static int findMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+
+        }
+            else
+                list.add(false);
+
+        return list;
+            if (candies[i] + extraCandies >= max)
+                list.add(true);
+
+        for (int i = 0; i < candies.length; i++) {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> list = new ArrayList<>(); 
+        int max = findMax(candies);
+class Solution {
+[
