1fecb76d33b2c1ef93a58ec7336586614a14e360
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-03-27 20:55:40 -0700

    Create file

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -0,0 +1,18 @@
+                        if (str.charAt(low) != str.charAt(high)) {
+                   
+                    
+                    while (low < high) {
+                if (str.charAt(0) == str.charAt(str.length() - 1)) {
+                    int low = 0, high = str.length() - 1;
+                str = s.substring(i, j + 1);
+
+                boolean isPalindrome = true;
+            String str = "";
+            for (int j = i; j < s.length(); j++) {
+
+        for (int i = 0; i < s.length(); i++) {
+    public int countSubstrings(String s) {
+        
+        List<String> list = new ArrayList<>();
+class Solution {
+"
