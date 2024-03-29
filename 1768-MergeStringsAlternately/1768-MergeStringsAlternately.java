4b13c760db856b67ae3277985226b72315b46d35
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-03-29 16:51:12 -0700

    Update file 3/30/2024, 7:48:34 AM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -1,20 +1,18 @@
-            } else {
-                p1++;
-                res += word2.charAt(p2);
-                p2++;
-            }
-
+            res += word1.charAt(w1++);
+        
+        while (w1 < word1.length())
             count++;
         }
+                res += word2.charAt(w2++);
+                
+                res += word1.charAt(w1++);
+            else
+        while (w1 < word1.length() && w2 < word2.length()) {
+            if (count % 2 == 0)  
+        int count = 0, w1 = 0, w2 = 0;
+        
+    public String mergeAlternately(String word1, String word2) {
 
-            if (count % 2 == 0) {
-                res += word1.charAt(p1);
-        while (p1 < word1.length() && p2 < word2.length()) {
-        while (p1 < word1.length()) {
-            res += word1.charAt(p1);
-            p1++;
-        }
-
-        while (p2 < word2.length()) {
-            res += word2.charAt(p2);
+        String res = "";
+class Solution {
 "
