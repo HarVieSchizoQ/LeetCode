0d320de69a514a8e8e3509ad11217ff5e5f22243
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-03-30 18:59:17 -0700

    Create file

---
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -0,0 +1,17 @@
+        counter = right.length - 2;
+        prod = 1;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) { 
+            prod *= nums[i];
+            left[counter++] = prod;
+        int prod = 1;
+
+
+        int counter = 1;
+        left[0] = 1;
+        right[right.length - 1] = 1;
+        int[] right = new int[nums.length];
+        int[] res = new int[nums.length];
+
+[
