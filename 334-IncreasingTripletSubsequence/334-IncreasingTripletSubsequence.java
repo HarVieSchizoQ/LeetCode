5b17e89a3f3510954fb4e2ae72a3f3f45709e136
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-04-02 03:05:51 -0700

    Create file

---
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.java
@@ -0,0 +1,16 @@
+                right = num;
+                
+                mid = num;
+                left = num;
+            else if (num <= mid) {
+            
+            if (num <= left) 
+        
+        for (int num: nums) { 
+                right = mid;
+            } else 
+    public boolean increasingTriplet(int[] nums) {
+
+        int left = Integer.MAX_VALUE, mid = Integer.MAX_VALUE, right = Integer.MAX_VALUE;
+class Solution {
+[
