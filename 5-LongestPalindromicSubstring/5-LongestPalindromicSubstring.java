5d7af67ce522ba22aaedc192a79236a013d5d310
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-04-06 19:55:42 -0700

    Create file

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -0,0 +1,17 @@
+            }
+        }
+                if (isPalindrome(current)) 
+                    res = current.length() > res.length() ? current: res;
+            for (int j = i; j < s.length(); j++) {
+                current = s.substring(i, j + 1);
+                
+
+        return res;
+    }
+
+    public boolean isPalindrome(String current) {
+        int low = 0, high = current.length() - 1;
+                    
+        while (low < high) {
+             if (current.charAt(low) != current.charAt(high)) 
+"
