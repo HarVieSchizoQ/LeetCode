808737f9adbaec7dd9527d167891c0184fb11552
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-04-07 02:42:56 -0700

    Create file

---
+++ 1679-MaxNumberofK-SumPairs/1679-MaxNumberofK-SumPairs.java
@@ -0,0 +1,13 @@
+                right--;
+            else
+                left++;       
+        }
+                left++;
+            } else if (res > k)
+                right--; 
+
+            if (res == k) {
+                count++;
+        while (left < right) {
+            int res = nums[left] + nums[right];
+[
