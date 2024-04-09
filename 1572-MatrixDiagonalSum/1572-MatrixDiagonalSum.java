53fd8398eceb9ad9da4134957f4bf91e7d45caab
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-04-09 03:41:25 -0700

    Create file

---
+++ 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.java
@@ -0,0 +1,11 @@
+                if (j != mat.length / 2)
+                    sum += mat[j++][i];         
+                else
+                    j++;
+            }        
+        }
+
+        return sum;
+    }
+}
+[
