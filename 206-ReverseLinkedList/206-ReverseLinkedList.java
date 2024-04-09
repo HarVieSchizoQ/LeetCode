e05ba1c1ae937f536b99025149a53ff0b48502f3
ShinjiCodeEVA <116258847+ShinjiCodeEVA@users.noreply.github.com>
2024-04-09 00:04:45 -0700

    Create file

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -0,0 +1,13 @@
+            current.next = prev;
+            current = next;
+        }
+
+        head = prev;
+        return head;
+     }        
+        while (current != null) { 
+            ListNode next = current.next;
+        ListNode prev = null;
+
+            prev = current; 
+[
